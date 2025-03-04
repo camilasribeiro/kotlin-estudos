ecfd4ea2d79700a01a2d020925e005fb34b209c5
Camila Ribeiro <camilas.ribeiro@outlook.com>
2025-03-04 00:24:49 -0300

    Exercícios Passo 2 - Condições Básicas

--- exercicios-faculdade/src/passo2/condicoesbasicas/Ex09.kt
+++ exercicios-faculdade/src/passo2/condicoesbasicas/Ex09.kt
@@ -17,9 +17,3 @@ fun main() {
         println("Não é possível formar um triângulo!")
     }
 }
-
-//[DESAFIO] Crie um programa que leia o tamanho de três segmentos
-//de reta. Analise seus comprimentos e diga se é possível formar um
-//triângulo com essas retas. Matematicamente, para três segmentos
-//formarem um triângulo, o comprimento de cada lado deve ser menor
-//que a soma dos outros dois.
